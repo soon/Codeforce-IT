c624e7ab14df0ca0e796cc6efee85e874129b89f
Andrew Kuchev <0coming.soon@gmail.com>
2015-09-27 16:58:30 +0500

    Added initial `ProblemViewSet`

--- codeforce_it/apps/codeforces_wrapper/views/__init__.py
+++ codeforce_it/apps/codeforces_wrapper/views/__init__.py
@@ -1,5 +1,6 @@
 from .user_view_set import UserViewSet
 from .group_view_set import GroupViewSet
+from .problem_view_set import ProblemViewSet
 
 
 __author__ = 'Andrew Kuchev (kuchevad@gmail.com)'
---
+++ codeforce_it/apps/codeforces_wrapper/views/problem_view_set.py
@@ -0,0 +1,11 @@
+from rest_framework import viewsets
+
+from codeforce_it.apps.codeforces_wrapper.serializers import ProblemSerializer
+
+
+__author__ = 'Andrew Kuchev (kuchevad@gmail.com)'
+
+
+class ProblemViewSet(viewsets.ModelViewSet):
+    queryset = Problem.objects.all()
+    serializer_class = ProblemSerializer
