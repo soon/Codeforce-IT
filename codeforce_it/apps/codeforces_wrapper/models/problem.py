c4f5371cc4c4c6f369951ff41a7cab3cdf189544
Andrew Kuchev <0coming.soon@gmail.com>
2015-09-27 16:40:33 +0500

    Added `contest` field to `Problem` model

--- codeforce_it/apps/codeforces_wrapper/models/problem.py
+++ codeforce_it/apps/codeforces_wrapper/models/problem.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
+from codeforce_it.apps.codeforces_wrapper.models import Contest
+
+
 __author__ = 'Andrew Kuchev (kuchevad@gmail.com)'
 
 
@@ -8,3 +11,4 @@ class Problem(models.Model):
     cf_contest_id = models.IntegerField(verbose_name=_('Codeforces contest number'))
     cf_index = models.CharField(verbose_name=_('Codeforces problem index'), max_length=16)
     max_score = models.FloatField(verbose_name=_('Maximum amount of points'))
+    contest = models.ForeignKey(Contest, null=True)
