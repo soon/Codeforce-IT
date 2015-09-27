948bf1b7606f13d1b58870d74b92481a5b28719b
Andrew Kuchev <0coming.soon@gmail.com>
2015-09-27 17:03:17 +0500

    Added initial `ContestantSerializer`

--- codeforce_it/apps/codeforces_wrapper/serializers/__init__.py
+++ codeforce_it/apps/codeforces_wrapper/serializers/__init__.py
@@ -1,6 +1,7 @@
 from .user_serializer import UserSerializer
 from .group_serializer import GroupSerializer
 from .problem_serializer import ProblemSerializer
+from .contestant_serializer import ContestantSerializer
 
 
 __author__ = 'Andrew Kuchev (kuchevad@gmail.com)'
---
+++ codeforce_it/apps/codeforces_wrapper/serializers/contestant_serializer.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+
+from codeforce_it.apps.codeforces_wrapper.models import Contestant
+
+
+__author__ = 'Andrew Kuchev (kuchevad@gmail.com)'
+
+
+class ContestantSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Contestant
+        fields = ('cf_handle',)
