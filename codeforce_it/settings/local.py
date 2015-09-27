779ad2363590535d8b7143b6a8742469025cc0be
Andrew Kuchev <0coming.soon@gmail.com>
2015-09-27 14:57:23 +0500

    Added local `SECRET_KEY` and `DATABASES` credentials

--- codeforce_it/settings/local.py
+++ codeforce_it/settings/local.py
@@ -15,3 +15,17 @@ DEBUG_TOOLBAR_CONFIG = {
     'SHOW_TEMPLATE_CONTEXT': True,
     'HIDE_DJANGO_SQL': False,
 }
+
+
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', '=#)0ha_k_v)k*uzdp1puh!o%2\=-q\=%-2y!q+3n(y^z44#ezu&v')
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('DATABASE_NAME', 'codeforce_it_db'),
+        'USER': os.getenv('DATABASE_USER', 'codeforce_it_db_user'),
+        'PASSWORD': os.getenv('DATABASE_PASSWORD', 'ctnpmj78zZtSHXKV'),
+        'HOST': os.getenv('DATABASE_HOST', '127.0.0.1'),
+        'PORT': os.getenv('DATABASE_PORT', '5432'),
+    }
+}
