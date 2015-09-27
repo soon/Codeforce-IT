a06b9099ef9d3dcc70b6008545e6282bd26db3b3
Andrew Kuchev <0coming.soon@gmail.com>
2015-09-27 16:37:12 +0500

    Added initial `Contest` field

--- codeforce_it/apps/codeforces_wrapper/models/__init__.py
+++ codeforce_it/apps/codeforces_wrapper/models/__init__.py
@@ -1,4 +1,5 @@
 from .contestant import Contestant
+from .contest import Contest
 from .problem import Problem
 from .submission import Submission
 
---
+++ codeforce_it/apps/codeforces_wrapper/models/contest.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+from codeforce_it.apps.codeforces_wrapper.models import Contestant
+
+
+__author__ = 'Andrew Kuchev (kuchevad@gmail.com)'
+
+
+class Contest(models.Model):
+    start_time = models.DateTimeField(_('Start time'))
+    duration = models.DurationField(_('Duration'))
+    contestants = models.ManyToManyField(Contestant)
