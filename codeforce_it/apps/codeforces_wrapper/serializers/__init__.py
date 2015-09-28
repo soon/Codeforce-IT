e5dc1d03fc3d8034fb22612b2874c1eed56aa2bf
Andrew Kuchev <0coming.soon@gmail.com>
2015-09-28 08:19:45 +0500

    Added initial `SubmissionSerializer`

--- codeforce_it/apps/codeforces_wrapper/serializers/__init__.py
+++ codeforce_it/apps/codeforces_wrapper/serializers/__init__.py
@@ -3,6 +3,7 @@ from .group_serializer import GroupSerializer
 from .problem_serializer import ProblemSerializer
 from .contestant_serializer import ContestantSerializer
 from .contest_serializer import ContestSerializer
+from .submission_serializer import SubmissionSerializer
 
 
 __author__ = 'Andrew Kuchev (kuchevad@gmail.com)'
