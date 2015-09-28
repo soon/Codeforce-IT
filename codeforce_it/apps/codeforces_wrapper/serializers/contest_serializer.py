5d445fee8cdf38bfaf9d30b4d4b2735c7aef43fa
Andrew Kuchev <0coming.soon@gmail.com>
2015-09-28 10:25:54 +0500

    Added `url` field to `ContestSerializer`

--- codeforce_it/apps/codeforces_wrapper/serializers/contest_serializer.py
+++ codeforce_it/apps/codeforces_wrapper/serializers/contest_serializer.py
@@ -9,5 +9,5 @@ __author__ = 'Andrew Kuchev (kuchevad@gmail.com)'
 class ContestSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Contest
-        fields = ('start_time', 'duration', 'contestants', 'end_time')
+        fields = ('url', 'start_time', 'duration', 'contestants', 'end_time')
         read_only_fields = ('end_time',)
