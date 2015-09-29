3c93d40de37d08dca5ec38e2e72b05bc0ad4e69c
Andrew Kuchev <0coming.soon@gmail.com>
2015-09-29 08:54:06 +0500

    Fixed submission loader scripts:

--- codeforce_it/apps/codeforces_wrapper/scripts/submissions_loader.py
+++ codeforce_it/apps/codeforces_wrapper/scripts/submissions_loader.py
@@ -3,6 +3,7 @@ import operator
 from datetime import datetime
 
 from codeforces import CodeforcesAPI, VerdictType
+from django.utils.timezone import make_aware, get_default_timezone
 
 from codeforce_it.apps.codeforces_wrapper.models import Submission
 
@@ -10,18 +11,27 @@ from codeforce_it.apps.codeforces_wrapper.models import Submission
 __author__ = 'Andrew Kuchev (kuchevad@gmail.com)'
 
 
+def group_by_to_dict_of_sets(iterable, key=None, value_selector=None):
+    value_selector = value_selector or (lambda x: x)
+    return dict(map(lambda k_v: (k_v[0], set(map(value_selector, k_v[1]))), groupby(iterable, key=key)))
+
+
 def retrieve_contest_submissions(contest):
-    problems = dict(groupby(lambda p: p.cf_contest_id, contest.problem_set.order_by('cf_contest_id')))
-    return chain.from_iterable(map(lambda contestant: retrieve_contestant_submissions(contestant, contest, problems),
-                                   contest.contestants.all()))
+    problems = group_by_to_dict_of_sets(contest.problem_set.order_by('cf_contest_id'),
+                                        key=lambda p: p.cf_contest_id,
+                                        value_selector=lambda p: p.cf_index)
+    return list(
+        chain.from_iterable(map(lambda contestant: retrieve_contestant_submissions(contestant, contest, problems),
+                                contest.contestants.all())))
 
 
 def store_only_new_submissions(submissions):
-    first_submission = min(submissions, key=lambda s: s.creation_time)
-    stored_submission_ids = set(map(operator.itemgetter('cf_id'),
-                                    Submission.objects.filter(creation_time__gte=first_submission.creation_time)))
-    for submission in filter(lambda s: s.id not in stored_submission_ids):
-        Submission.from_json(submission).save()
+    if submissions:
+        first_submission = min(submissions, key=lambda s: s.creation_time)
+        stored_submission_ids = set(map(operator.itemgetter('cf_id'),
+                                        Submission.objects.filter(creation_time__gte=first_submission.creation_time)))
+        for submission in filter(lambda s: s.id not in stored_submission_ids, submissions):
+            Submission.from_json(submission).save()
 
 
 def load_submissions_to_db(contests):
@@ -32,7 +42,9 @@ def load_submissions_to_db(contests):
 
 def retrieve_contestant_submissions(contestant, contest, problem_indices_by_contest_id):
     api = CodeforcesAPI()
-    return list(filter(lambda s: (datetime.fromtimestamp(s.creation_time) >= contest.start_time and
+    return list(filter(lambda s: (make_aware(datetime.fromtimestamp(s.creation_time),
+                                             get_default_timezone()) >= make_aware(contest.start_time,
+                                                                                   get_default_timezone()) and
                                   s.verdict != VerdictType.testing and
                                   s.problem.index in problem_indices_by_contest_id.get(s.problem.contest_id, [])),
                        api.user_status(contestant.cf_handle)))
--- codeforce_it/apps/codeforces_wrapper/tests/scripts/test_submissions_loader.py
+++ codeforce_it/apps/codeforces_wrapper/tests/scripts/test_submissions_loader.py
@@ -6,7 +6,8 @@ import codeforces
 
 from django.test import TestCase
 
-from codeforce_it.apps.codeforces_wrapper.scripts.submissions_loader import retrieve_contestant_submissions
+from codeforce_it.apps.codeforces_wrapper.scripts.submissions_loader import retrieve_contestant_submissions, \
+    store_only_new_submissions, retrieve_contest_submissions
 from codeforce_it.apps.codeforces_wrapper import models
 
 
@@ -55,3 +56,55 @@ class SubmissionsLoaderTests(TestCase):
                                                 duration=timedelta(days=7))
         submissions = retrieve_contestant_submissions(contestant, contest, {580: ['A', 'C']})
         self.assertEquals(3, len(submissions))
+
+    def test_store_only_new_submissions__no_submissions(self):
+        store_only_new_submissions([])
+
+    @mock.patch.object(codeforces.CodeforcesAPI, 'user_status')
+    def test_retrieve_contest_submissions(self, mock_user_status):
+        mock_user_status.return_value = [
+            codeforces.Submission('''{"id":13158455,"contestId":580,"creationTimeSeconds":1442942706,
+            "relativeTimeSeconds":3306,"problem":{"contestId":580,"index":"B","name":"Кефа и компания",
+            "type":"PROGRAMMING","points":1250.0,"tags":["binary search","sortings","two pointers"]},
+            "author":{"contestId":580,"members":[{"handle":"soon"}],
+            "participantType":"CONTESTANT","ghost":false,"room":142,"startTimeSeconds":1442939400},
+            "programmingLanguage":"GNU C++11","verdict":"OK","testset":"TESTS",
+            "passedTestCount":35,"timeConsumedMillis":561,"memoryConsumedBytes":4096000}'''),
+            codeforces.Submission('''{"id":13156276,"contestId":580,"creationTimeSeconds":1442942124,
+            "relativeTimeSeconds":2724,"problem":{"contestId":580,"index":"C",
+            "name":"Кефа и парк","type":"PROGRAMMING","points":1500.0,"tags":["dfs and similar","trees"]},
+            "author":{"contestId":580,"members":[{"handle":"soon"}],"participantType":"CONTESTANT","ghost":false,
+            "room":142,"startTimeSeconds":1442939400},"programmingLanguage":"GNU C++11","verdict":"OK",
+            "testset":"TESTS","passedTestCount":40,"timeConsumedMillis":623,"memoryConsumedBytes":21504000}'''),
+            codeforces.Submission('''{"id":13153676,"contestId":580,"creationTimeSeconds":1442941466,
+            "relativeTimeSeconds":2066,"problem":{"contestId":580,"index":"C","name":"Кефа и парк",
+            "type":"PROGRAMMING","points":1500.0,"tags":["dfs and similar","trees"]},
+            "author":{"contestId":580,"members":[{"handle":"soon"}],"participantType":"CONTESTANT","ghost":false,
+            "room":142,"startTimeSeconds":1442939400},"programmingLanguage":"GNU C++11","verdict":"WRONG_ANSWER",
+            "testset":"PRETESTS","passedTestCount":7,"timeConsumedMillis":15,"memoryConsumedBytes":0}'''),
+            codeforces.Submission('''{"id":13148475,"contestId":580,"creationTimeSeconds":1442940297,
+            "relativeTimeSeconds":896,"problem":{"contestId":580,"index":"B","name":"Кефа и компания",
+            "type":"PROGRAMMING","points":1250.0,"tags":["binary search","sortings","two pointers"]},
+            "author":{"contestId":580,"members":[{"handle":"soon"}],"participantType":"CONTESTANT","ghost":false,
+            "room":142,"startTimeSeconds":1442939400},"programmingLanguage":"GNU C++11","verdict":"WRONG_ANSWER",
+            "testset":"PRETESTS","passedTestCount":13,"timeConsumedMillis":108,"memoryConsumedBytes":512000}'''),
+            codeforces.Submission('''{"id":13144872,"contestId":580,"creationTimeSeconds":1442939683,
+            "relativeTimeSeconds":283,"problem":{"contestId":580,"index":"A","name":"Кефа и первые шаги",
+            "type":"PROGRAMMING","points":750.0,"tags":["brute force","dp","implementation"]},
+            "author":{"contestId":580,"members":[{"handle":"soon"}],"participantType":"CONTESTANT","ghost":false,
+            "room":142,"startTimeSeconds":1442939400},"programmingLanguage":"GNU C++11","verdict":"OK",
+            "testset":"TESTS","passedTestCount":28,"timeConsumedMillis":249,"memoryConsumedBytes":0}''')
+        ]
+
+        contestant = models.Contestant.objects.create(cf_handle='soon')
+        contest = models.Contest.objects.create(start_time=datetime.fromtimestamp(1442900000),
+                                                duration=timedelta(days=7))
+        contest.contestants.add(contestant)
+
+        models.Problem.objects.create(cf_contest_id=580, cf_index='A', max_score=500, contest=contest)
+        models.Problem.objects.create(cf_contest_id=580, cf_index='C', max_score=500, contest=contest)
+
+        submissions = retrieve_contest_submissions(contest)
+
+        self.assertIsInstance(submissions, list)
+        self.assertEqual(3, len(submissions))
