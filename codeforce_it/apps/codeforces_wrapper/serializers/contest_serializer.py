81982960fdbb7c40edcafed35a3b6c49a7e98ae3
Andrew Kuchev <0coming.soon@gmail.com>
2015-09-29 10:14:50 +0500

    Added `id` field to `ContestSerializer`

--- codeforce_it/apps/codeforces_wrapper/serializers/contest_serializer.py
+++ codeforce_it/apps/codeforces_wrapper/serializers/contest_serializer.py
@@ -9,5 +9,5 @@ __author__ = 'Andrew Kuchev (kuchevad@gmail.com)'
 class ContestSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Contest
-        fields = ('url', 'start_time', 'duration', 'contestants', 'end_time')
+        fields = ('url', 'id', 'start_time', 'duration', 'contestants', 'end_time')
         read_only_fields = ('end_time',)
