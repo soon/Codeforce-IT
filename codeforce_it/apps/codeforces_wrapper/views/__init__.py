2c8b15ad2c80605d5c4f677fcd07017930fd83ee
Andrew Kuchev <0coming.soon@gmail.com>
2015-09-29 08:56:12 +0500

    Added `CronJobLogViewSet`

--- codeforce_it/apps/codeforces_wrapper/views/__init__.py
+++ codeforce_it/apps/codeforces_wrapper/views/__init__.py
@@ -4,6 +4,7 @@ from .problem_view_set import ProblemViewSet
 from .contestant_view_set import ContestantViewSet
 from .contest_view_set import ContestViewSet
 from .submission_view_set import SubmissionViewSet
+from .cron_job_log_view_set import CronJobLogViewSet
 
 
 __author__ = 'Andrew Kuchev (kuchevad@gmail.com)'
