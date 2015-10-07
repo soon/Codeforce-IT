bc0c8bce7fbb8df23025ce7c402e6832a6fc01f1
Andrew Kuchev <0coming.soon@gmail.com>
2015-10-07 12:28:12 +0500

    Added `production.py` config

---
+++ codeforce_it/settings/production.py
@@ -0,0 +1,29 @@
+from codeforce_it.settings.base import *
+
+
+DEBUG = False
+
+ALLOWED_HOSTS = ['ALLOWED_HOSTS']
+
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('DATABASE_NAME'),
+        'USER': os.getenv('DATABASE_USER'),
+        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
+        'HOST': os.getenv('DATABASE_HOST', '127.0.0.1'),
+        'PORT': os.getenv('DATABASE_PORT', '5432'),
+    }
+}
+
+
+# Use the cached template loader so template is compiled once and read from
+# memory instead of reading from disk on each load.
+TEMPLATES[0]['OPTIONS']['loaders'] = [
+    ('django.template.loaders.cached.Loader', [
+        'django.template.loaders.filesystem.Loader',
+        'django.template.loaders.app_directories.Loader',
+    ]),
+]
